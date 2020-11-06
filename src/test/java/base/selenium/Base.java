4975ef969e68b72126226e3ddff083c0b4ed4837
agustin colque <agustin.colque@gmail.com>
2020-11-05 20:46:18 -0400

    buy product

--- src/test/java/base/selenium/Base.java
+++ src/test/java/base/selenium/Base.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.util.List;
 import base.selenium.enums.Browsers;
 
 public class Base {
@@ -24,63 +23,13 @@ public class Base {
             IBrowserConnection browserDriver = browserConnection.getBrowserConnection(browser);
             this.driver = browserDriver.driverConnection(headless);
             this.actions = new Actions(driver);
-            this.wait = new WebDriverWait(driver,30);
+            this.wait = new WebDriverWait(driver,60);
         }catch (Exception e){
             System.out.println("The message is: " + e.getMessage());
             System.out.println("The cause is: " + e.getCause() );
             e.printStackTrace();
         }
         return driver;
-    }
-
-    public void moveToElement(By locator){
-        actions.moveToElement(driver.findElement(locator))
-                .perform();
-    }
-
-    public void changeTab(int indexTab){
-        String[] browserTibSize = driver.getWindowHandles().toArray(new String[0]);
-        driver.switchTo().window(browserTibSize[indexTab]);
-    }
-
-    public void alertAccept() {
-        driver.switchTo().alert().accept();
-    }
-
-    public void alertDismiss() {
-        driver.switchTo().alert().dismiss();
-    }
-
-    public String getAlertMessage() {
-        return driver.switchTo().alert().getText();
-    }
-
-    public void jsClick(By locator) {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click();",driver.findElement(locator));
-    }
-
-    public WebElement findElement(By locator){
-        return driver.findElement(locator);
-    }
-
-    public List<WebElement> findElements(By locator){
-        return driver.findElements(locator);
-    }
-
-    public String getText(WebElement element){
-        return element.getText();
-    }
-
-    public void dragAndDropElement(By locatorDrag, By locatorDrop){
-        actions.dragAndDrop(driver.findElement(locatorDrag),driver.findElement(locatorDrop))
-                .perform();
-    }
-
-    public void datePicker(By locator, String date){
-        driver.findElement(locator).sendKeys(date);
-        driver.findElement(locator).sendKeys(Keys.RETURN);
-
     }
 
     public void waitElementVisible(By locator){
@@ -97,34 +46,10 @@ public class Base {
     public void type(String inputText, By locator){
         driver.findElement(locator).sendKeys(inputText);
     }
-    public void typeInFrame(String inputText, By locator){
-
-        driver.switchTo().frame(0);
-        driver.findElement(locator).sendKeys(inputText);
-    }
 
     public void click(By locator){
         driver.findElement(locator).click();
     }
-
-    public void click(WebElement element){
-        element.click();
-    }
-    public void pressEnterKey(By locator){
-        driver.findElement(locator).sendKeys(Keys.ENTER);
-    }
-
-    public Boolean isDisplayed(By locator){
-        try {
-            return driver.findElement(locator).isDisplayed();
-        }catch (org.openqa.selenium.NoSuchElementException e){
-            return false;
-        }
-    }
-    public Boolean isEnable(By locator){
-        return driver.findElement(locator).isEnabled();
-    }
-
     public void goTo(String url){
         driver.get(url);
     }
